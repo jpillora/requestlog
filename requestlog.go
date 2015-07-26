16ad974670777624b4018a3236577c0dcad9e0d8
Jaime Pillora <dev@jpillora.com>
2015-07-26 20:17:56 +1000

    refactor

--- requestlog.go
+++ requestlog.go
@@ -18,27 +18,35 @@ var TimeFormat = "2006/01/02 15:04:05.000"
 
 func Wrap(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		m := New(w)
+		m := New(w, r)
 		next.ServeHTTP(m, r)
-		m.Log(r)
+		m.Log()
 	})
 }
 
-func New(w http.ResponseWriter) *MonitorableWriter {
+func New(w http.ResponseWriter, r *http.Request) *MonitorableWriter {
+	ip := r.Header.Get("X-Forwarded-For")
+	if ip == "" {
+		ip, _, _ = net.SplitHostPort(r.RemoteAddr)
+	}
 	return &MonitorableWriter{
-		t0: time.Now(),
-		w:  w,
+		t0:     time.Now(),
+		w:      w,
+		method: r.Method,
+		path:   r.URL.Path,
+		ip:     ip,
 	}
 }
 
 //monitorable ResponseWriter
 type MonitorableWriter struct {
 	t0 time.Time
-	//
+	//handler
 	w http.ResponseWriter
 	//stats
-	Code int
-	Size int64
+	method, path, ip string
+	Code             int
+	Size             int64
 }
 
 func (m *MonitorableWriter) Header() http.Header {
@@ -58,31 +66,23 @@ func (m *MonitorableWriter) WriteHeader(c int) {
 var integerRegexp = regexp.MustCompile(`\.\d+`)
 
 //replace ResponseWriter with a monitorable one, return logger
-func (m *MonitorableWriter) Log(r *http.Request) {
-
+func (m *MonitorableWriter) Log() {
 	now := time.Now()
-
-	ip := r.Header.Get("X-Forwarded-For")
-	if ip == "" {
-		ip, _, _ = net.SplitHostPort(r.RemoteAddr)
-	}
 	if m.Code == 0 {
 		m.Code = 200
 	}
-	size := sizestr.ToString(m.Size)
-
 	b := bytes.Buffer{}
 	b.WriteString(now.Format(TimeFormat) + " ")
-	b.WriteString(r.Method + " ")
-	b.WriteString(r.URL.Path + " ")
+	b.WriteString(m.method + " ")
+	b.WriteString(m.path + " ")
 	b.WriteString(strconv.Itoa(m.Code) + " ")
 	dur := integerRegexp.ReplaceAllString(now.Sub(m.t0).String(), "")
-	b.WriteString(dur + " ")
-	if size != "0B" {
-		b.WriteString(size + " ")
+	b.WriteString(dur)
+	if m.Size > 0 {
+		b.WriteString(" " + sizestr.ToString(m.Size))
 	}
-	if ip != "::1" && ip != "127.0.0.1" {
-		b.WriteString("(" + ip + ")")
+	if m.ip != "::1" && m.ip != "127.0.0.1" {
+		b.WriteString(" (" + m.ip + ")")
 	}
 	b.WriteString("\n")
 	Writer.Write(b.Bytes())
