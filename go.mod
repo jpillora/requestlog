63add2e4ad69e7ede7cf1cffd938b8ac84492af5
Jaime Pillora <dev@jpillora.com>
2024-01-01 21:21:22 +1100

    progress

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/jpillora/requestlog/v2
+module github.com/jpillora/requestlog
 
 go 1.21.5
 
@@ -7,15 +7,19 @@ go 1.21.5
 require (
 	github.com/jpillora/jplog v1.0.2
 	github.com/jpillora/sizestr v1.0.0
+	github.com/stretchr/testify v1.8.4
 	github.com/tomasen/realip v0.0.0-20180522021738-f0c99a92ddce
 )
 
 require (
 	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mattn/go-isatty v0.0.18 // indirect
 	github.com/mattn/go-runewidth v0.0.14 // indirect
 	github.com/muesli/termenv v0.15.2 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
 	golang.org/x/sys v0.7.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- v2/rlogger_test.go
+++ v2/rlogger_test.go
@@ -1,13 +1,14 @@
-package requestlog_test
+package requestlog
 
 import (
 	"bytes"
 	"net/http"
 	"net/http/httptest"
+	"regexp"
 	"testing"
 
 	"github.com/jpillora/jplog"
-	"github.com/jpillora/requestlog/v2"
+	"github.com/stretchr/testify/require"
 )
 
 func TestOut(t *testing.T) {
@@ -21,11 +22,18 @@ func TestOut(t *testing.T) {
 
 	b := bytes.Buffer{}
 
-	wrap := requestlog.New(h, requestlog.Options{
+	wrap := New(h, Options{
 		Logger: jplog.New(&b),
 	})
 
 	wrap.ServeHTTP(w, req)
 
-	t.Log(b.String())
+	out := stripAnsi(b.String())
+	require.Contains(t, out, `INFO OK method=GET path=/foo code=200`)
+}
+
+var ansire = regexp.MustCompile("[\u001B\u009B][[\\]()#;?]*(?:(?:(?:[a-zA-Z\\d]*(?:;[a-zA-Z\\d]*)*)?\u0007)|(?:(?:\\d{1,4}(?:;\\d{0,4})*)?[\\dA-PRZcf-ntqry=><~]))")
+
+func stripAnsi(str string) string {
+	return ansire.ReplaceAllString(str, "")
 }
